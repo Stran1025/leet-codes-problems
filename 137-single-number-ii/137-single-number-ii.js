730f96f2460d81a8257f71e7afcc251ce5f0137d
Sang Tran <sang.tran1025@gmail.com>
2022-07-16 21:20:52 -0700

    Time: 85 ms (74.45%), Space: 42.4 MB (89.13%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    nums.sort();
+    for (let i = 0; i < nums.length; i+= 3) {
+        if (nums[i] !== nums[i+2] || nums[i] !== nums[i+2]) {
+            return nums[i]
+        }
+    }
+};
